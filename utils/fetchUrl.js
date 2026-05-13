7e36a8755d9a82825a2e5d818b725d5720320a72
Alex Capon <caplex.work@gmail.com>
2026-05-13 23:35:54 +0700

    branch inited

---
+++ utils/fetchUrl.js
@@ -0,0 +1,15 @@
+export default async function fetchJSON(url) {
+    try {
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Ошибка при получении данных:', error);
+        throw error;
+    }
+}
