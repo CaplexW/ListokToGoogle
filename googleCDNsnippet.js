3b2a85e31e828eeb291d5dacf824f2fa24f6461c
Caplex <165675467+CaplexW@users.noreply.github.com>
2026-05-18 15:00:33 +0700

    Update googleCDNsnippet.js
    Замена google.id на google.oauth

--- googleCDNsnippet.js
+++ googleCDNsnippet.js
@@ -1,4 +1,4 @@
-// Инициализация Firebase
+// Инициализация Firebase (если нужно)
 import { initializeApp } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-app.js";
 import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-analytics.js";
 
@@ -15,17 +15,34 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-function handleCredentialResponse(response) {
-  const accessToken = response.credential;
-  console.log("Google OAuth Token:", accessToken);
-  localStorage.setItem("googleAccessToken", accessToken);
-  alert("Авторизация успешна! Токен сохранён в localStorage.");
+// Инициализация OAuth 2.0 клиента для получения access_token
+let tokenClient;
+
+function initClient() {
+  tokenClient = google.accounts.oauth2.initTokenClient({
+    client_id: "862650729174-t93uout29b5g7uah5kg3j9et0p9g949p.apps.googleusercontent.com",
+    scope: "https://www.googleapis.com/auth/calendar",
+    callback: (tokenResponse) => {
+      const accessToken = tokenResponse.access_token;
+      console.log("Access Token:", accessToken);
+      localStorage.setItem("googleAccessToken", accessToken);
+      alert("Авторизация успешна! Токен сохранён.");
+    },
+  });
 }
 
+// Обработчик для кнопки авторизации
+function handleAuthClick() {
+  tokenClient.requestAccessToken();
+}
+
+// Инициализация при загрузке страницы
 window.onload = function () {
+  initClient();
+  // Рендерим кнопку Google Sign-In (опционально, если хочешь оставить старую кнопку)
   google.accounts.id.initialize({
     client_id: "862650729174-t93uout29b5g7uah5kg3j9et0p9g949p.apps.googleusercontent.com",
-    callback: handleCredentialResponse,
+    callback: handleAuthClick, // Теперь вызывает requestAccessToken
     scope: "https://www.googleapis.com/auth/calendar",
     auto_select: false,
     cancel_on_tap_outside: false,
